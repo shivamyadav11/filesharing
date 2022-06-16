768bbd2571376c6c423e4a058f2c6df681ac14f8
Shivam kumar yadav <79139812+shivamyadav11@users.noreply.github.com>
2022-06-17 00:30:26 +0530

    Update server.js
    second

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-Cors 
+//Cors 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
@@ -18,7 +18,7 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 
-//app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
