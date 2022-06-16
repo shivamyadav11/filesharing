db08752f96f500b02946943482222b8e29c3210a
Shivam kumar yadav <79139812+shivamyadav11@users.noreply.github.com>
2022-06-17 03:58:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 // }
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3001', 'https://shivamyadav11.github.io/fileshareFron']
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3001', 'https://shivamyadav11.github.io/fileshareFron/']
 }));
 
 
